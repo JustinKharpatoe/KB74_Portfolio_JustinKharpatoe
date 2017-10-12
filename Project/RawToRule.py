94ade620150774aa28a57be0ce2c86c0eafe6c4e
Justin Kharpatoe <justinkharpatoe@hotmail.nl>
2017-10-12 15:05:09 +0200

    Convert object kolom naar float
    Twee kolommen converteren van 'object' naar 'float'

--- Project/RawToRule.py
+++ Project/RawToRule.py
@@ -3,11 +3,14 @@
 RAW .csv file to Rule .csv file
 
 """
+import time
 import pandas as pd
 
+start_time = time.time()
+print("Rules aanmaken...")
 TempRule = {"Normaal": [19, 23]}
 PIRRule = {"Constant": [0.5, 1]}
-CO2Rule = {"Normaal": [0, 1299], "Hoog": [1300, 1999], "Extreem": [2000], "Constant": 3}
+CO2Rule = {"Normaal": [0, 999], "Hoog": [1000, 1999], "Extreem": [2000], "Constant": 3}
 AirRule = {"GoedeErvaring": [0, 0.15]}
 #OccRule = {"NietAanwezig": 0, "Aanwezig": 1}
 
@@ -16,10 +19,23 @@ Writefile = Readfile[:-4] + "_RULE" + Readfile[-4:]
 
 RAWbestand = pd.read_csv(Readfile, sep=";", index_col=0)
 
-print("Bezig met bewerken...")    
+for i in range(0, len(RAWbestand)):
+    RAWbestand.iloc[i, 0] = RAWbestand.iloc[i, 0].replace(",", ".")
+    RAWbestand.iloc[i, 3] = RAWbestand.iloc[i, 3].replace(",", ".")
+    
+RAWbestand["Temperature"] = pd.to_numeric(RAWbestand["Temperature"], errors="coerce")
+RAWbestand["Air flow"] = pd.to_numeric(RAWbestand["Air flow"], errors="coerce")
+
+#vorigePIRwaarde = 0
+temperatuurWaarden = []
+CO2Waarden = []
+
+print("Bezig met data bewerken...")    
 for column in range(0, len(RAWbestand.columns)):
+    print("\tKolom %d..." % (column+1))    
     for row in range(0, len(RAWbestand)):
         if column == 0:     #B
+            temperatuurWaarden.append(RAWbestand.iloc[row, column])
             if RAWbestand.iloc[row, column] in range(TempRule["Normaal"][0], TempRule["Normaal"][1]+1):
                 RAWbestand.iloc[row, column] = 0
             else:
@@ -29,7 +45,20 @@ for column in range(0, len(RAWbestand.columns)):
                 RAWbestand.iloc[row, column] = 1
             else:
                 RAWbestand.iloc[row, column] = 0
+#==============================================================================
+#             if row == 0:
+#                 vorigePIRwaarde = RAWbestand.iloc[row, column]
+#                 RAWbestand.iloc[row, column] = 1
+#             else:
+#                 if RAWbestand.iloc[row, column] == vorigePIRwaarde:
+#                     vorigePIRwaarde = RAWbestand.iloc[row, column]
+#                     RAWbestand.iloc[row, column] = 0
+#                 else:
+#                     vorigePIRwaarde = RAWbestand.iloc[row, column]
+#                     RAWbestand.iloc[row, column] = 1
+#==============================================================================
         if column == 2:     #D
+            CO2Waarden.append(RAWbestand.iloc[row, column])
             if RAWbestand.iloc[row, column] in range(CO2Rule["Normaal"][0], CO2Rule["Normaal"][1]+1):
                 RAWbestand.iloc[row, column] = 0
             elif RAWbestand.iloc[row, column] in range(CO2Rule["Hoog"][0], CO2Rule["Hoog"][1]+1):
@@ -42,11 +71,15 @@ for column in range(0, len(RAWbestand.columns)):
             else:
                 RAWbestand.iloc[row, column] = 0
         if column == 4:     #F
-            if RAWbestand.iloc[row, column]:
+            if temperatuurWaarden[row] > TempRule["Normaal"][1] and CO2Waarden[row] >= 700:
                 RAWbestand.iloc[row, column] = 0
             else:
                 RAWbestand.iloc[row, column] = 1
 
-print("Eerste vijf regels:")
-print(RAWbestand.head())
+print("Eerste 3 regels:")
+print(RAWbestand.head(3))
+print("\nLaatste 3 regels:")
+print(RAWbestand.tail(3))
 RAWbestand.to_csv(Writefile)
+
+print("\n====== Execution time: %s ======" % (time.time() - start_time))
